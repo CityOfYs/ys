d36b2a734f8bdab931bf1b0662c3e309347c4bf4
AllaHoffman <sm1th303@gmail.com>
2022-09-26 17:47:09 -0400

    svgs are boned in jsx now woo

--- src/YsNavLogo.js
+++ src/YsNavLogo.js
@@ -1,19 +1,13 @@
-import logo from './images/logo.svg';
 import './App.css';
 import Navbar from 'react-bootstrap/Navbar'
 import React, { Component } from "react";
+import SvgComponent from './Icon.js';
 
 class YsNavLogo extends Component {
   render () {
     return <div>
       <Navbar.Brand href="#home">
-        <img
-          alt=""
-          src={logo}
-          width="60"
-          height="60"
-          className="d-inline-block"
-        />{' '}
+        <SvgComponent/>
         <div id="city-brand"><strong>YS</strong> {this.props.tagline} </div>
       </Navbar.Brand>
     </div>
